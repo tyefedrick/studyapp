3fc45a262ef765990177951b4c019e676b3d3899
Tyler Fedrick <tyefedrick@gmail.com>
2024-11-22 11:14:18 -0500

    Navbar now has a get started button and the title of the website now takes you back to the main page when clicked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :require_authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :user_signed_in?
+
+  private
+
+  def user_signed_in?
+    # Replace this with your actual logic to check if a user is logged in
+    session[:user_id].present?
+  end
 end
